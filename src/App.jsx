afd40e2ab8d7e238d51cad61a9feca53300536ac
cs0041 <pamzalove20@hotmail.com>
2023-01-06 23:16:12 +0700

    add Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Swap, Stake, Transfer, Faucet } from './components'
-import { Route, Routes } from 'react-router-dom'
+import {Link, Route, Routes, Navigate } from 'react-router-dom'
 
 import React, { useState,useEffect, useContext } from 'react'
 import { TransactionContext } from './context/TransactionContext'
@@ -19,19 +19,19 @@ const App = () => {
    }, [notification])
 
   return (
-    
     <div className="relative flex ">
       <div className="w-full h-full flex flex-col overflow-y-scroll  bg-[#0D111C]   fixed">
-          <Navbar />
-        
+        <Navbar />
+
         <div className=" flex flex-col ">
           <div className="flex-1  pb-40 justify-center items-center">
             <Routes>
-              <Route path="/" element={<Swap />} />
               <Route path="/swap" element={<Swap />} />
               <Route path="/stake" element={<Stake />} />
               <Route path="/transfer" element={<Transfer />} />
               <Route path="/faucet" element={<Faucet />} />
+              <Route path="/" element={<Navigate to="/swap" />} />
+            
             </Routes>
           </div>
         </div>
@@ -45,5 +45,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App
